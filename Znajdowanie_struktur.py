cbec204c11f2fdc86ab15799fbff58679525d0e6
AnnaGlinka <aglinka8@gmail.com>
2016-05-15 00:26:40 +0200

    Add files via upload

--- Znajdowanie_struktur.py
+++ Znajdowanie_struktur.py
@@ -9,6 +9,8 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
     pojedynczyLancuch = []
     petla = []
     wybrzuszenie = []
+    skrzyzowanie = []
+    ciag = []
     for i, j in enumerate(RNA_kropkowo_nawiasowa):
             mapaWiazan[i] = None
     for i, j in enumerate(RNA_kropkowo_nawiasowa):
@@ -21,40 +23,60 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
             mapaWiazan[para2] = para1
           
 
-    #print(mapaWiazan)
-    spinka = re.compile('[(][.]{1,500}[)]')
+    print(mapaWiazan)
+
+    stos.clear()
+   
     kropka = re.compile('[.]{1,1000}')
+    st = re.compile('[(]{1,1000}')
 
-    print (mapaWiazan)
+    
+    stosIterator = st.finditer(RNA_kropkowo_nawiasowa)
+    
+    for s in stosIterator:
+        stos.append(s.span())
 
-    spinkaIterator = spinka.finditer(RNA_kropkowo_nawiasowa)
-    for sp in spinkaIterator:
-        spinkiDoWlosow.append(sp.span())
-    for sp in spinkiDoWlosow:
-        print("spinka", sp)
+    #for s in stos:
+        #print(s[0], s[1])
+
+   
+    
 
     kropkiIterator = kropka.finditer(RNA_kropkowo_nawiasowa)
     for kr in kropkiIterator:
+        #print(kr)
         kropki.append(kr.span())
-    for kr in kropki:
-        print("kropki", kr)
+    #for kr in kropki:
+     #   print("kropki", kr)
 
     for kr in kropki:
         poczatek = kr[0] #pierwszy z pary kropek
         koniec = kr[1] #drugi z pary kropek
-       
-        #for sp in spinkiDoWlosow:
-         #   print("spinka", sp[0], sp[1])
-       
+
         if poczatek == 0 or koniec>len(RNA_kropkowo_nawiasowa)-1:
             pojedynczyLancuch.append((poczatek,koniec-1))
             continue
+       
+        #for sp in spinkiDoWlosow:
+         #   print("spinka", sp[0], sp[1])
+        if (RNA_kropkowo_nawiasowa[poczatek-1]==')' and RNA_kropkowo_nawiasowa[koniec]=='('):
+            #print("poczatek-1", poczatek-1, mapaWiazan.get(poczatek-1,  'None'))
+            #print(mapaWiazan.get(3,  'None'))
+            #print(kropki.index(a int table table, 5))
+            skrzyzowanie.append((poczatek, koniec-1))
+            continue
+
+       
+        
+        if (RNA_kropkowo_nawiasowa[poczatek-1]=='(' and RNA_kropkowo_nawiasowa[koniec]==')') :
+           spinkiDoWlosow.append((poczatek, koniec-1))
+           continue
+           
+           
         if (RNA_kropkowo_nawiasowa[poczatek-1]=='(' and RNA_kropkowo_nawiasowa[koniec]=='(') :
             if mapaWiazan.get(poczatek-1,  'None') -  mapaWiazan.get(koniec,  'None') == 1:
                 wybrzuszenie.append((poczatek, koniec-1))
-                print("wybrzuszenie")
             else:
-                print("petla")
                 petla.append((poczatek, koniec-1))
            
             #print("poczatek-1", poczatek-1, mapaWiazan.get(poczatek-1,  'None'))
@@ -64,31 +86,45 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
         if (RNA_kropkowo_nawiasowa[poczatek-1]==')' and RNA_kropkowo_nawiasowa[koniec]==')'):
             if mapaWiazan.get(poczatek-1,  'None') -  mapaWiazan.get(koniec,  'None') == 1:
                 wybrzuszenie.append((poczatek, koniec-1))
-                print("wybrzuszenie")
             else:
-                print("petla")
                 petla.append((poczatek, koniec-1))
-            '''
-            if mapaWiazan.get(poczatek-1,  'None') -  mapaWiazan.get(koniec,  'None') == 1:
-                wybrzuszenie.append((poczatek, koniec-1))
-                print("wybrzuszenie")
-            else:
-                print("petla")
-                petla.append((poczatek, koniec-1))
-            '''
-           
-          
-            #petla.append((poczatek, koniec-1))
-
             continue
-        '''
-        if ((RNA_kropkowo_nawiasowa[poczatek-1]==')' and RNA_kropkowo_nawiasowa[koniec]==')') or (RNA_kropkowo_nawiasowa[poczatek-1]=='(' and RNA_kropkowo_nawiasowa[koniec]=='(')):
-            petla.append((poczatek, koniec-1))
-        '''
 
+    for sk in skrzyzowanie:
+        poczatek_sk = sk[0]
+        koniec_sk = sk[1]
+        poprzednie_sk = mapaWiazan.get(poczatek_sk-1,  'None') - 1
+        for pet in petla:
+            if pet[1] == poprzednie_sk:
+                skrzyzowanie.append((pet[0],pet[1]))
+                petla.remove((pet[0],pet[1]))
+
+    skrzyzowanie.sort()
+    
+    print(RNA_kropkowo_nawiasowa)
     print("pojedynczy lancuch", pojedynczyLancuch)
+    print("spinkiDoWlosow", spinkiDoWlosow)
     print("petla", petla)
     print("wybrzuszenie", wybrzuszenie)
+    print("skrzyzowanie", skrzyzowanie)
+    print("stos", stos)
+
+    #for x in RNA_kropkowo_nawiasowa:
+        
+
+    ciag.append(pojedynczyLancuch)
+    ciag.append(spinkiDoWlosow)
+    ciag.append(petla)
+    ciag.append(wybrzuszenie)
+    ciag.append(skrzyzowanie)
+    #ciag.append(stos)
+    ciag.sort()
+    print("ciag", ciag)
+
+    stri = ""
+
+    
+    
     
 
 
@@ -103,8 +139,9 @@ if __name__ == "__main__":
      #elementyStruktury('..((((((...)))(((...)))(((...)))))).')
      elementyStruktury('..((..(((..((..))..)))..))..')
      print("druga")
-     elementyStruktury('..((..(((((..))..)))..))..')
+     #elementyStruktury('..((..(((((..))..)))..))..')
      #elementyStruktury('..((..(((..((..)))))..))..')
+     elementyStruktury('(((..(((...)))...(((...)))..)))')
     # elementyStruktury('..(((...(((.....))))))...')
     # elementyStruktury('(((((((((...((((((.........))))))........((((((.......))))))..)))))))))')
     
