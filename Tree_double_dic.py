3b642279cacf4b32ea99b1551c71bd71720edc04
AnnaGlinka <aglinka8@gmail.com>
2016-05-19 19:17:08 +0200

    Add files via upload

--- Tree_double_dic.py
+++ Tree_double_dic.py
@@ -4,7 +4,6 @@
 def dfs(graf, start):
     odwiedzone, stos, dfs_tab = set(), [start], []
    
-    
     while stos:
         wierzcholek = stos.pop()
        
@@ -15,11 +14,24 @@ def dfs(graf, start):
             stos.extend(graf[wierzcholek] - odwiedzone)
     return dfs_tab
 
-def utworzDrzewo(elementy):
-    slownikDoDrzewa = {-1: 'korzen'}
+def utworzDrzewo(elem):
+
+    elementy = ""
+    for e in elem:
+        if e != 'd':
+            elementy += e
+
+    slownikDoDrzewa = {-1: ['korzen', 1]}
     drzewo = {-1: set([])}
     for ind, e in enumerate(elementy):
-        slownikDoDrzewa.update({ind: e})
+        #print("e=", e)
+        slownikDoDrzewa.update({ind: [e, 1]})
+        
+        
+        if elementy[ind-1] == 's':
+            drzewo.update({ind-1: set([])})
+            return drzewo, slownikDoDrzewa
+        
         if ind == 0:
             #print("ind", ind)
             drzewo.update({-1: set([ind])})
@@ -31,25 +43,44 @@ def utworzDrzewo(elementy):
 
     return drzewo, slownikDoDrzewa
 
+
+
 def odtworzElementy(seq, slownik):
     print("seq", seq)
     print("slownik", slownik)
     for ind, s in enumerate(seq):
+        print(s, slownik[ind-1])
+        '''
         if ind == 0:
             continue
+   
         else:
             print(s, slownik[ind-1])
+        '''
 
 
 
+print('ldpdpdsdpdpdl')
 d = (utworzDrzewo('ldpdpdsdpdpdl')[0])
 seq = dfs(d, -1)
 
-#print(seq)
+print(seq)
 
-#print(utworzDrzewo('ldpdpdsdpdpdl')[0])
+print(utworzDrzewo('ldpdpdsdpdpdl')[0])
 
 #print(utworzDrzewo('ldpdpdsdpdpdl')[1])
 
 slownik = (utworzDrzewo('ldpdpdsdpdpdl')[1])
 odtworzElementy(seq, slownik)
+
+
+print("-------------------")
+str2 = 'dxdsdxdsdxd'
+print(str2)
+
+d2 = (utworzDrzewo(str2)[0])
+slownik2 = (utworzDrzewo(str2)[1])
+seq2 = dfs(d2, -1)
+
+print(seq2)
+odtworzElementy(seq2, slownik2)
