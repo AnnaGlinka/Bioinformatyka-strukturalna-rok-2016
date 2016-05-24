3881df19bae74bc0fe858d59d74c604b6b66f696
AnnaGlinka <aglinka8@gmail.com>
2016-05-25 01:19:24 +0200

    Add files via upload

--- DFS_z_nawiasow.py
+++ DFS_z_nawiasow.py
@@ -6,11 +6,15 @@ def utworzDrzewo(elementy):
     wyjscie = ""
     licznikNawiasow = 0
     maxLicznikNawiasow = 0
+    licznikX = 1
     while ind < len(elementy):
-        print(elementy[ind], licznikNawiasow)
+        #print(elementy[ind], licznikNawiasow)
 
         if(elementy[ind] == '['):
             wyjscie += 'x'
+            #wyjscie += str(licznikX)
+            licznikX+=1
+            
         else:
             wyjscie +=elementy[ind]
 
@@ -21,8 +25,9 @@ def utworzDrzewo(elementy):
             
             #wyjscie += 's'
             while elementy[ind] != ']':
+                #print("------", elementy[ind], licznikNawiasow)
                 if ind >= len(elementy)-1:
-                      print("wyj", wyjscie)
+                      #print("wyj", wyjscie)
                       return wyjscie
                 #print("w while z ]", elementy[ind])
                 if elementy[ind] == '[':
@@ -36,8 +41,9 @@ def utworzDrzewo(elementy):
         if elementy[ind] == ']':
             licznikNawiasow -=1
             while elementy[ind] != '[':
+                #print("<<<<", elementy[ind], licznikNawiasow)
                 if ind >= len(elementy)-1:
-                      print("wyj", wyjscie)
+                      #print("wyj", wyjscie)
                       return wyjscie
                 #print("w while z [...", elementy[ind])
                 ind += 1
@@ -61,9 +67,9 @@ def utworzDrzewo(elementy):
       
 
 
-str = 'pp[pp[s][ppspp]pp][s][pp[psp][psp]pp][pp[s][s]pp]pp' 
+str1 = 'pp[pp[s][ppspp]pp][s][pp[psp][psp]pp][pp[s][s]pp]pp' 
 str2 = 'psp'
 wyj = ""
-d = utworzDrzewo(str)
-utworzDrzewo(str2)
+print(utworzDrzewo(str1))
+print(utworzDrzewo(str2))
 #print(d)
