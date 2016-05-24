ca5bf1d9ab3db205ad412f622db6ac8310b35665
AnnaGlinka <aglinka8@gmail.com>
2016-05-25 01:00:10 +0200

    Add files via upload

--- DFS_z_nawiasow.py
+++ DFS_z_nawiasow.py
@@ -1,36 +1,4 @@
-'''
 
-def utworzDrzewo(elementy, wyj):
-        if len(wyj)<=1:
-            print(wyj)
-            return wyj
-
-        print(elementy)
-        print("wyj", wyj)
-
-        #licznikNawiasow = 0
-      
-
-        while ind < len(elementy):
-            print(elementy[ind], ind)
-
-           
-            if elementy[ind] =='[':
-                print("mam [")
-                wyj += 'x'
-                utworzDrzewo(ind+1, elementy, wyj)
-
-            
-           
-           
-
-            wyj += elementy[ind]
-            ind +=1
-       
-
-        #----------------------------------------------
-
-'''
 
 def utworzDrzewo(elementy):
     print(elementy)
@@ -39,33 +7,51 @@ def utworzDrzewo(elementy):
     licznikNawiasow = 0
     maxLicznikNawiasow = 0
     while ind < len(elementy):
+        print(elementy[ind], licznikNawiasow)
+
+        if(elementy[ind] == '['):
+            wyjscie += 'x'
+        else:
+            wyjscie +=elementy[ind]
+
+        if elementy[ind] == '[':
+            licznikNawiasow +=1
        
         if elementy[ind] == 's':
             
-            wyjscie += 's'
+            #wyjscie += 's'
             while elementy[ind] != ']':
                 if ind >= len(elementy)-1:
                       print("wyj", wyjscie)
                       return wyjscie
-                print("w while z ]", elementy[ind])
+                #print("w while z ]", elementy[ind])
+                if elementy[ind] == '[':
+                   licznikNawiasow +=1
+                if elementy[ind] == ']':
+                   licznikNawiasow -=1
                 ind += 1
-            print("po while z ]")   
+            #print("po while z ]")   
+      
 
         if elementy[ind] == ']':
             licznikNawiasow -=1
-           
             while elementy[ind] != '[':
                 if ind >= len(elementy)-1:
                       print("wyj", wyjscie)
                       return wyjscie
-                print("w while z [...", elementy[ind])
-                if elementy[ind] != ']':
-                    licznikNawiasow -=1
+                #print("w while z [...", elementy[ind])
                 ind += 1
-            print("po while z [...")   
+                if elementy[ind] == '[':
+                   licznikNawiasow +=1
+                if elementy[ind] == ']':
+                   licznikNawiasow -=1
+               
+                
+            #print("po while z [...")   
 
         #print(elementy[ind], ind)
-        wyjscie += elementy[ind]
+        
+        #wyjscie += elementy[ind]
         ind +=1
 
 
