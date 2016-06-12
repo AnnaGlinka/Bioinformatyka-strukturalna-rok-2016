c92fcb7b3250365ccd5845860959cc4a059164e5
AnnaGlinka <aglinka8@gmail.com>
2016-06-12 22:35:17 +0200

    Add files via upload

--- Znajdowanie_struktury_3.py
+++ Znajdowanie_struktury_3.py
@@ -34,17 +34,16 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
                 mapaWiazan[para2] = para1
             else:
                 print("nieprawidlowe dane, za duzo nawiasow zamykajacych")
+                print("--------------------------------------------------")
                 return
           
-    #print(mapaWiazan)
+    
     if len(stos):
         print("nieprawidlowe dane, za duzo nawiasow otwierajacych")
+        print("--------------------------------------------------")
         return
   
-    
-
     stos.clear()
-   
     kropka = re.compile('[.]{1,1000}')
     st = re.compile('[()]{1,1000}')
     spinka_bez_kropek = re.compile('[(][)]')
@@ -54,40 +53,25 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
     for skbk in skrzyzowanie_bez_kropekIterator:
         skrzyzowanieBezKropek.append((skbk.span()))
         x = skbk.span()
-        #print(skbk.span())
-        #print("mapa",  mapaWiazan.get(x[0],  'None'), mapaWiazan.get(x[1]-1,  'None'))
-        
-
+     
     l = len(skrzyzowanieBezKropek)
 
-   
     if l > 0:
-        #print( skrzyzowanieBezKropek[0], mapaWiazan.get(skrzyzowanieBezKropek[0][0]))
-        #print( skrzyzowanieBezKropek[l-1], mapaWiazan.get(skrzyzowanieBezKropek[l-1][1]-1))
         skrzyzowanieBezKropek.append((mapaWiazan.get(skrzyzowanieBezKropek[0][0]), mapaWiazan.get(skrzyzowanieBezKropek[0][0])))
         skrzyzowanieBezKropek.append((mapaWiazan.get(skrzyzowanieBezKropek[l-1][1]-1), mapaWiazan.get(skrzyzowanieBezKropek[l-1][1]-1)))
-        
-       
-    skrzyzowanieBezKropek.sort()
-
+    
     spinka_bez_kropekIterator = spinka_bez_kropek.finditer(RNA_krn);
     for sbk in spinka_bez_kropekIterator:
         spinkaBezKr.append(sbk.span());
 
-    
     stosIterator = st.finditer(RNA_krn)
     for s in stosIterator:
         stos.append(s.span())
-        #print(stos)
-
-   
+      
 
     kropkiIterator = kropka.finditer(RNA_krn)
     for kr in kropkiIterator:
-        #print(kr)
         kropki.append(kr.span())
-    #for kr in kropki:
-     #   print("kropki", kr)
 
     for kr in kropki:
         poczatek = kr[0] #pierwszy z pary kropek
@@ -96,18 +80,9 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
         if poczatek == 0 or koniec>len(RNA_krn)-1:
             pojedynczyLancuch.append((poczatek,koniec-1))
             continue
-       
-        #for sp in spinkiDoWlosow:
-         #   print("spinka", sp[0], sp[1])
         if (RNA_krn[poczatek-1]==')' and RNA_krn[koniec]=='('):
-            #print("poczatek-1", poczatek-1, mapaWiazan.get(poczatek-1,  'None'))
-            #print("mapa ",mapaWiazan.get(3,  'None'))
-            #print(kropki.index(a int table table, 5))
             skrzyzowanie.append((poczatek, koniec-1))
             continue
-
-       
-        
         if (RNA_krn[poczatek-1]=='(' and RNA_krn[koniec]==')') :
            spinkiDoWlosow.append((poczatek, koniec-1))
            continue
@@ -118,10 +93,7 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
                 wybrzuszenie.append((poczatek, koniec-1))
             else:
                 petla.append((poczatek, koniec-1))
-           
-            #print("poczatek-1", poczatek-1, mapaWiazan.get(poczatek-1,  'None'))
-            #print("koniec", koniec, mapaWiazan.get(koniec,  'None'))
-            #petla.append((poczatek, koniec-1))
+          
             continue
         if (RNA_krn[poczatek-1]==')' and RNA_krn[koniec]==')'):
             #print("do wyb z prawej", mapaWiazan.get(poczatek-1,  'None'), mapaWiazan.get(koniec,  'None')  )
@@ -131,7 +103,7 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
             else:
                 petla.append((poczatek, koniec-1))
             continue
-
+    skrzyzowanieBezKropek.sort()
     for sk in skrzyzowanie:
         poczatek_sk = sk[0]
         koniec_sk = sk[1]
@@ -143,15 +115,7 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
 
     skrzyzowanie.sort()
     
-    '''
-    print(RNA_kropkowo_nawiasowa)
-    print("pojedynczy lancuch", pojedynczyLancuch)
-    print("spinkiDoWlosow", spinkiDoWlosow)
-    print("petla", petla)
-    print("wybrzuszenie", wybrzuszenie)
-    print("skrzyzowanie", skrzyzowanie)
-    print("stos", stos)
-    '''
+   
     # r - oznaczenie jak dla spinki do wlosow, ale bez niesparowanych nukleotydow ((()))
     # z - odpowiednik do wybrzuszenia pojawiajacego siê po prawej stronie
     # o - skrzyzowanie bez niesparowanych nukleotydow pomiedzy odgalezieniami
@@ -183,6 +147,7 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
     stri2 = ""
 
     for ind, x in enumerate(ciag):
+
         if x in skrzyzowanieBezKropek:
            
             if mapaWiazan.get(x[0]) < x[0] and mapaWiazan.get(x[1]) > x[1]:
@@ -218,8 +183,6 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
             stri2 += "w"
             continue
         if x in skrzyzowanie:
-            #print("x in skrzyzowanie", x, ciag[ind+1][0], mapaWiazan.get(ciag[ind+1][0]))
-            #print("x in skrzyzowanie drugi nawias", x, ciag[ind-1][1], mapaWiazan.get( ciag[ind-1][1]))
             stri += "x"*(x[1]-x[0]+1)
             #stri2 += "x"
             if ciag[ind+1][0] < mapaWiazan.get(ciag[ind+1][0]) and ciag[ind-1][1] > mapaWiazan.get( ciag[ind-1][1]):
@@ -247,26 +210,19 @@ def elementyStruktury(RNA_kropkowo_nawiasowa):
     
     print("----------------------------------------------")
 
-    #-----------------------
-    
-
     return stri2, mapaWiazan
         
-
-    
-
 if __name__ == "__main__":
 
      elementyStruktury('((([[..)))..]]')
      elementyStruktury('((..(())..))')
      elementyStruktury('(((((..(())..)))..))')
      elementyStruktury('......(((.{[[....[[)))...].].}.]]..')
-     elementyStruktury('.((((((..(((.....[....)))..((((.......))))......(((((..]....))))))))))).')
-     elementyStruktury('.((((((..(((....(((....)))...)))..((((.......))))......(((((..]....))))))))))).')
+     elementyStruktury('.((((((..(((.....[....)))..((((.......))))......(((((..]....)))))..)))))).')
+     elementyStruktury('.((((((..(((....(((....)))...)))..((((.......))))......(((((..]....)))))..)))))).')
      elementyStruktury('..((((...(((..)))..)))(((....(((...)))...)))...')
      elementyStruktury('..((((..((..))..))((..((..))..))))...')
-     elementyStruktury('..(((((((((...)))....)))...)))')
-     elementyStruktury('....((((((..((((........)))).(((((.......)))))).....(((((.......))))))))))..')
+     elementyStruktury('..(((.((((((...)))....)))....)))')
      elementyStruktury('..((((((...)))(((...)))(((...)))(((...)))(((...)))(((...)))(((...)))))).')
      elementyStruktury('..((((((...)))(((...)))..(((...)))(((...)))(((...)))(((...)))(((...)))))).')
      elementyStruktury('..((((((...)))(((..((..(())..))..((..(())..))..)))..(((...)))(((...)))(((...)))(((...)))(((...)))))).')
