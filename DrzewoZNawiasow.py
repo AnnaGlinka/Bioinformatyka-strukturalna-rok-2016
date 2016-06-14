84227c56a3504fed525fc79eba0cfcb973bce1f6
AnnaGlinka <aglinka8@gmail.com>
2016-06-14 20:56:11 +0200

    Add files via upload

--- DrzewoZNawiasow.py
+++ DrzewoZNawiasow.py
@@ -1,17 +1,13 @@
 ﻿
-def ZbudujDrzewo(elem):
+def drzewoDFS(elem):
     print("wejscie: ", elem)
-    #if elem[0] == "[":
-    #   elem = elem[1:]
-    #if elem[-1] == "]":
-    #    elem =  elem[:-1]
+  
     i = 0
     wyjscie = ""   
     stos = []
     stosGalezi = []
     licznikNawiasow = 0 #zlicza ile razy stos nawiasow stal sie pusty
     galaz = ""
-    #-------------------------------------------------------------
     
     if (elem[0] == 'l' or elem[-1] == 'l'):
         wyjscie+='1'
@@ -20,10 +16,8 @@ def ZbudujDrzewo(elem):
         
         if (elem[i] == 'p' or elem[i] == 'w' or elem[i] == 'z'):
                 wyjscie+='1'
-                #print("dodaje 1")
         if (elem[i] == 's' or elem[i] =='r'):
             wyjscie+='0'
-            #print("dodaje 0")
             while elem[i] != ']' and i < len(elem)-1:
                i +=1
 
@@ -34,9 +28,6 @@ def ZbudujDrzewo(elem):
                 if elem[i] == '[':
                     licznikNawiasow +=1
                 galaz += elem[i]
-
-                
-                
 
                 if licznikNawiasow == 0:
                     if len(galaz) > 1:
@@ -49,8 +40,6 @@ def ZbudujDrzewo(elem):
                     galaz = ""
                 i+=1
 
-        #print("stosGalezi", stosGalezi)
-
 
         if len(stosGalezi):
            licznikStosu = len(stosGalezi)
@@ -58,8 +47,7 @@ def ZbudujDrzewo(elem):
 
         while stosGalezi:
             e = stosGalezi.pop()
-            #print("e=========================", e)
-           
+
             ind = 0
             licznikNawiasow = 0
             licznikStosu2 = 0
@@ -67,15 +55,12 @@ def ZbudujDrzewo(elem):
             while ind < len(e):
                if (e[ind] == 'p' or e[ind] == 'w' or e[ind] == 'z'):
                    wyjscie+='1'
-                   #print("dodaje 1")
+                  
                if (e[ind] == 's' or e[ind] =='r'):
                   wyjscie+='0'
-                  #print("dodaje 0")
+                 
                   while e[ind] != ']' and ind < len(e)-1:
                       ind +=1
-                
-               
-                
 
                elif (e[ind] == '['):
                    while ind < len(e):
@@ -92,17 +77,11 @@ def ZbudujDrzewo(elem):
                           galaz = ""
                        ind+=1
                ind+=1
-            #print("licznik stosu", licznikStosu)
-            #print("licznik stosu2", licznikStosu2)
             if licznikStosu2 != 0:
                 wyjscie+=str(licznikStosu2)
-                #print("dodaje ", licznikStosu2)
-            
-
+             
         i +=1
 
-
-        #print(wyjscie)
     return wyjscie
       
 str1 = 'lprpl' 
@@ -110,7 +89,7 @@ str2 = 'zprpw'
 str3 = 'l[s][[s]][s]l'
 #PDB_00176
 str4 = 'l[psp][[psp]][[psp]][psp]'
-str5 = 'p[s][[s]][s]l'
+
 str6 = 'lpz[pzp[s][p[s][pp[s][s]pp]p]pwp][[s]][[pp[psp][psp]pp]][[pp[s][s]pp]][pp[s][s]pp]wpl'
 str7 = 'pz[pzp[s][ppspp]pwp][[zsw]][[pp[psp][psp]pp]][pp[s][s]pp]wpl'
 #PDB_00547 
@@ -125,27 +104,27 @@ str11 = 'l[psp][ppspp]'
 
 str12 = ' l[s][wws]l'
 
-#print("dfs: ", ZbudujDrzewo(str1))
-#print("dfs: ", ZbudujDrzewo(str2))
-#print("dfs: ", ZbudujDrzewo(str3))
+print("dfs: ", drzewoDFS(str1))
+print("dfs: ", drzewoDFS(str2))
+print("dfs: ", drzewoDFS(str3))
 
 #PDB_00176
-#print("dfs: ", ZbudujDrzewo(str4))
-print("wyjscie: ", ZbudujDrzewo(str5))
+print("dfs: ", drzewoDFS(str4))
+
 #sek12
-#print("dfs: ", ZbudujDrzewo(str6))
+print("dfs: ", drzewoDFS(str6))
 #sek 11
-#print("dfs: ", ZbudujDrzewo(str7))
-#print("wyjscie: ", ZbudujDrzewo(str8))
+print("dfs: ", drzewoDFS(str7))
+print("wyjscie: ", drzewoDFS(str8))
 #PDB_00142
-#print("dfs: ", ZbudujDrzewo(str9))
+print("dfs: ", drzewoDFS(str9))
 
 # CRW_00552
-#print("dfs: ", ZbudujDrzewo(str10))
+print("dfs: ", drzewoDFS(str10))
 #PDB_00547 
-#print("dfs: ", ZbudujDrzewo(str11))
+print("dfs: ", drzewoDFS(str11))
 #RFA_00642
-#print("dfs: ", ZbudujDrzewo(str12))
+print("dfs: ", drzewoDFS(str12))
 
 
 
